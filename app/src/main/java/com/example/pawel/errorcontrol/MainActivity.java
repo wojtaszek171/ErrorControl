4b7c2fcb8f3b320f6d0d8ca390654bf12c1e010c
wojtaszek171 <pawelwojtaszko@o2.pl>
2018-04-07 13:48:58 +0200

    added dialogs for generate and disrupt

--- app/src/main/java/com/example/pawel/errorcontrol/MainActivity.java
+++ app/src/main/java/com/example/pawel/errorcontrol/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.pawel.errorcontrol;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +15,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +36,62 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.submethods_array, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown2.setAdapter(adapter2);
+
+        Button generate = findViewById(R.id.generate);
+        Button disrupt = findViewById(R.id.disrupt);
+        final TextView outputBits = findViewById(R.id.outputBits);
+        final TextView inputBits = findViewById(R.id.inputBits);
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.setTitle("Podaj liczbę bitów");
+                final String[] stringArray = new String[10];
+                int n=8;
+                for(int i=0; i<10; i++){
+                    stringArray[i] = Integer.toString(n);
+                    n+=8;
+                }
+                final NumberPicker picker = dialog.findViewById(R.id.numberPicker);
+                picker.setMaxValue(stringArray.length-1);
+                picker.setMinValue(0);
+                picker.setDisplayedValues(stringArray);
+                Button okButton = dialog.findViewById(R.id.ok_button);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        inputBits.setText(String.valueOf(picker.getValue()*8));
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+            }
+        });
+        disrupt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.setTitle("Podaj liczbę bitów");
+                final NumberPicker picker = dialog.findViewById(R.id.numberPicker);
+                picker.setMaxValue(20);
+                picker.setMinValue(0);
+                Button okButton = dialog.findViewById(R.id.ok_button);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        outputBits.setText(String.valueOf(picker.getValue()));
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+            }
+        });
     }
 
 }
