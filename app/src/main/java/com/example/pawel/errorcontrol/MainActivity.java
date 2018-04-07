5943198f1ef2ce05b325419a12b981e1fcb4865c
wojtaszek171 <pawelwojtaszko@o2.pl>
2018-04-07 22:06:43 +0200

    almost everything work

--- app/src/main/java/com/example/pawel/errorcontrol/MainActivity.java
+++ app/src/main/java/com/example/pawel/errorcontrol/MainActivity.java
@@ -14,51 +14,132 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Parity parityTransmitter = new Parity();
+    Hamming hammingTransmitter = new Hamming();
+    Crc crcTransmitter = new Crc();
+    Parity parityReceiver = new Parity();
+    Hamming hammingReceiver = new Hamming();
+    Crc crcReceiver = new Crc();
+    DataBits dataBits = new DataBits();
+    CodeBase transmitter =parityTransmitter;
+    CodeBase receiver = parityReceiver;
+    Button generate;
+    Button disrupt;
+    Button encode;
+    Button decode;
+     EditText outputBits;
+     EditText inputBits;
+     TextView outputBitsText;
+     TextView decodedBits;
+     TextView detectedBits;
+     TextView notDetected;
+     TextView bitsFixed;
+     TextView bitsSend;
+    TextView redundant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Parity parityTransmitter = new Parity();
-        final Hamming hammingTransmitter = new Hamming();
-        Crc crcTransmitter = new Crc();
-        //Crc32 crc32Transmitter = new Crc32();
-        Parity parityReceiver = new Parity();
-        Hamming hammingReceiver = new Hamming();
-        Crc crcReceiver = new Crc();
-        final DataBits dataBits = new DataBits();
-        final CodeBase transmitter=parityTransmitter;
-        CodeBase receiver=parityReceiver;
-
         Spinner dropdown = findViewById(R.id.methodSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.methods_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown.setAdapter(adapter);
-        Spinner dropdown2 = findViewById(R.id.subMethodSpinner);
+        final Spinner dropdown2 = findViewById(R.id.subMethodSpinner);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.submethods_array, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown2.setAdapter(adapter2);
 
-        Button generate = findViewById(R.id.generate);
-        Button disrupt = findViewById(R.id.disrupt);
-        Button encode = findViewById(R.id.codeButton);
-        final TextView outputBits = findViewById(R.id.outputBits);
-        final TextView inputBits = findViewById(R.id.inputBits);
+         generate = findViewById(R.id.generate);
+         disrupt = findViewById(R.id.disrupt);
+         encode = findViewById(R.id.codeButton);
+         decode = findViewById(R.id.decodeButton);
+        outputBits = findViewById(R.id.outputBits);
+        inputBits = findViewById(R.id.inputBits);
+        decodedBits = findViewById(R.id.outputDecodedBits);
+        detectedBits = findViewById(R.id.detected_text);
+        notDetected = findViewById(R.id.not_detected_text);
+        bitsFixed = findViewById(R.id.bits_fixed);
+        bitsSend = findViewById(R.id.bits_send);
+        redundant = findViewById(R.id.redundant_bits);
+        outputBitsText = findViewById(R.id.outputBitsText);
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                outputBits.setText("");
+                switch (i){
+                    case 0:
+                        transmitter = parityTransmitter;
+                        receiver = parityReceiver;
+                        dropdown2.setVisibility(View.INVISIBLE);
+                        break;
+                    case 1:
+                        transmitter = hammingTransmitter;
+                        receiver = hammingReceiver;
+                        dropdown2.setVisibility(View.INVISIBLE);
+                        break;
+                    case 2:
+                        transmitter = crcTransmitter;
+                        receiver = crcReceiver;
+                        dropdown2.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        dropdown2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                outputBits.setText("");
+                switch (i){
+                    case 0:
+                        crcTransmitter.setKey(Crc.CRC16);
+                        crcReceiver.setKey(Crc.CRC16);
+                        break;
+                    case 1:
+                        crcTransmitter.setKey(Crc.CRC32);
+                        crcReceiver.setKey(Crc.CRC32);
+                        break;
+                    case 2:
+                        crcTransmitter.setKey(Crc.SDLC);
+                        crcReceiver.setKey(Crc.SDLC);
+                        break;
+                    case 3:
+                        crcTransmitter.setKey(Crc.SDLC_REVERSE);
+                        crcReceiver.setKey(Crc.SDLC_REVERSE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final Dialog dialog = new Dialog(MainActivity.this);
                 dialog.setContentView(R.layout.custom_dialog);
                 dialog.setTitle("Podaj liczbę bitów");
+                ((TextView) dialog.findViewById(R.id.dialog_text)).setText("Liczba bitów do wygenerowania");
                 final String[] stringArray = new String[10];
                 int n=8;
                 for(int i=0; i<10; i++){
@@ -95,8 +176,8 @@ public class MainActivity extends AppCompatActivity {
                 okButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
-                        outputBits.setText(String.valueOf(picker.getValue()));
+                        transmitter.interfere(Integer.parseInt(String.valueOf(picker.getValue())));
+                        outputBits.setText(transmitter.codeToString());
                         dialog.dismiss();
                     }
                 });
@@ -125,10 +206,73 @@ public class MainActivity extends AppCompatActivity {
                 transmitter.setData(str);
                 transmitter.encode();
                 outputBits.setText(transmitter.codeToString());
+                outputBitsText.setText(transmitter.codeToString());
                 transmitter.setCode(transmitter.codeToString());
                 //receivedData.setText(transmitter.codeToString());
             }
         });
+        decode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transmitter.setCode(outputBits.getText().toString());
+                int errors=countErrors();
+                transmitter.fix();
+                //correctedData.setText(transmitter[0].codeToString());
+                //colorFixedBits(transmitter[0].getBitTypes());
+                transmitter.decode();
+                decodedBits.setText(transmitter.dataToString());
+                bitsSend.setText(Integer.toString(transmitter.getDataBitsNumber()));
+                redundant.setText(Integer.toString(transmitter.getControlBitsNumber()));
+                int detected = transmitter.getDetectedErrorsNumber();
+                detectedBits.setText(Integer.toString(detected));
+                bitsFixed.setText(Integer.toString(transmitter.getFixedErrorsNumber()));
+                notDetected.setText(Integer.toString(errors-detected));
+            }
+        });
+    }
+    private int countErrors()
+    {
+        String input = inputBits.getText().toString();
+        String output = decodedBits.getText().toString();
+        if (input.length()!=output.length()) return -1;
+        else
+        {
+            int errors=0;
+            int l=input.length();
+            for (int i=0; i<l; i++)
+            {
+                if (input.charAt(i)!=output.charAt(i)) errors++;
+            }
+            return errors;
+        }
     }
 
+//    public void colorFixedBits(int type[])
+//    {
+//        String str=correctedData.getText();
+//        if (str.length()==type.length)
+//        {
+//            StyledDocument doc = correctedData.getStyledDocument();
+//            Style style = correctedData.addStyle("colours", null);
+//
+//            correctedData.setText("");
+//            int l = type.length;
+//            for (int i=0; i<l; i++)
+//            {
+//                Color colour=Color.black;
+//                switch (type[i])
+//                {
+//                    case 0: colour=Color.green; break;
+//                    case 1: colour=Color.red; break;
+//                    case 2: colour=Color.orange; break;
+//                    case 3: colour=Color.cyan; break;
+//                    case 4: colour=Color.magenta; break;
+//                    case 5: colour=Color.yellow; break;
+//                }
+//                StyleConstants.setForeground(style, colour);
+//                try { doc.insertString(doc.getLength(), ""+str.charAt(i), style); }
+//                catch (BadLocationException e){}
+//            }
+//        }
+//    }
 }
